d8b2c27f27025ceb3a644bdc8cb7209645407eeb
Avinay Mehta <avinay1165@gmail.com>
2021-02-13 14:57:47 +0530

    force push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+  
+import React from 'react';
+import Game from './Components/Game'
+
+function App() {
+  return (
+    <Game/>
+  );
+}
+
+export default App;
